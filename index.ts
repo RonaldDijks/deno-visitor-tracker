98613f1916cc0437baa7e86faadf5e2e9538d62c
RonaldDijks <ronalddijks@protonmail.com>
2020-09-21 14:17:51 +0200

    add basic inmemory hit counting

--- index.ts
+++ index.ts
@@ -1,14 +1,22 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { parse } from 'https://deno.land/std/flags/mod.ts'
 
 const args = parse(Deno.args);
 const port = Number(args.port) || 8000;
 
-const app = new Application();
+let hits = 0;
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+const router = new Router();
+
+router.get('/', (ctx) => {
+  console.log(hits)
+  hits += 1;
+  ctx.response.body = hits;
 });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 await app.listen({ port }).then(() => {
   console.log(`Server started on port: 8000`)
